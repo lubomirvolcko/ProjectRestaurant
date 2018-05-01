fc9211d43ae74164fe72ad96c3f97f99b2492684
PavolKiss <32639696+PavolKiss@users.noreply.github.com>
2018-05-01 11:19:27 +0200

    added website

---
+++ RestaurantWEB/js/script.js
@@ -0,0 +1,49 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  $("a[href='#top']").click(function() {
+     $("html, body").animate({ scrollTop: 0 }, "slow");
+     return false;
+  });
+
+  $("a[href='#down']").click(function() {
+    var n = $(document).height();
+    $('html, body').animate({ scrollTop: n }, "slow");
+    return false;
+  });
+ 
+   $('#beers').click(function() {
+      $('#ulBeers').toggle("slow");
+   });
+
+   $('#hotDrinks').click(function() {
+      $('#hot-drinks').toggle("slow");
+   });
+
+
+})(jQuery); // End of use strict
