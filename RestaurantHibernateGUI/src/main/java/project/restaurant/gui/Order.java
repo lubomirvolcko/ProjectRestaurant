7c9b0ae0660f430fed5298da0480614d9efa4408
lubomirvolcko <lubomir.volcko@akademiasovy.sk>
2018-05-28 10:48:54 +0200

    round total price to 2 decimal

--- RestaurantHibernateGUI/src/main/java/project/restaurant/gui/Order.java
+++ RestaurantHibernateGUI/src/main/java/project/restaurant/gui/Order.java
@@ -1,6 +1,7 @@
 package project.restaurant.gui;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Order {
@@ -14,6 +15,22 @@ public class Order {
         this.numberOfItem = numberOfItem;
         this.nameItem = nameItem;
         this.price = price;
+        this.totalPrice = totalPrice;
+    }
+
+    public void setNumberOfItem(int numberOfItem) {
+        this.numberOfItem = numberOfItem;
+    }
+
+    public void setNameItem(String nameItem) {
+        this.nameItem = nameItem;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
         this.totalPrice = totalPrice;
     }
 
@@ -33,10 +50,26 @@ public class Order {
         return totalPrice;
     }
 
-    List<Order> orderArray = new ArrayList<>();
 
-    public void setToArrayList(){
+    ArrayList<Order> orderArray = new ArrayList<Order>();
+
+
+    public void setToArrayList(Order order){
+
+
+
+        System.out.println("Name of item: "+getNameItem());
+        System.out.println("Number of item: "+getNumberOfItem());
+        System.out.println("Price of item: "+getPrice());
+        System.out.println("Total price of item: "+totalPrice);
+
+        orderArray.add(order);
+
+        //System.out.println(order.getNameItem());
+        //System.out.println("ARRAY LIST 0: "+orderArray.get(0).getNumberOfItem()+" - "+orderArray.get(0).getNameItem()+" - "+orderArray.get(0).getPrice()+" - "+orderArray.get(0).getTotalPrice());
+        //System.out.println("ARRAY LIST 1: "+orderArray.get(1).getNumberOfItem()+" - "+orderArray.get(1).getNameItem()+" - "+orderArray.get(1).getPrice()+" - "+orderArray.get(1).getTotalPrice());
     }
+
 
 
 }
